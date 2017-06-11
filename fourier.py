c81194ceadfef3e64b4e12aac0195bb9836a89fc
griger <gustavo.rg92@gmail.com>
2017-06-12 01:43:01 +0200

    Corrigiendo cálculo de series de Fourier

--- fourier.py
+++ fourier.py
@@ -13,13 +13,12 @@ def bn (f, n, p = pi):
     return integrate(f*sin(n*t*pi/p), (t, -p, p)) / p
 
 def getFourierN (f, N, p = pi):
-    return an(f, 0) + summation(an(f,n) * cos(n*t*pi/p), (n, 1, N)) + summation(bn(f, n) * sin(n*t*pi/p), (n, 1, N))
+    return an(f, 0)/2 + summation(an(f,n) * cos(n*t*pi/p), (n, 1, N)) + summation(bn(f, n) * sin(n*t*pi/p), (n, 1, N))
 
 #Defino las funciones de los ejercicios
 
 # Ejercicio 1
 
-'''
 f = Piecewise((-1, t < 0), (1, t > 0), (0, True))
 S6 = getFourierN(f, 6)
 S7 = getFourierN(f, 7)
@@ -32,13 +31,14 @@ p1[2].line_color = 'green'
 p1[3].line_color = 'yellow'
 
 p1.show()
-'''
+
 
 # Ejercicio 2
 
+'''
 g = Piecewise((-t, t < 0), (t, 0 <= t), (0, True))
 
-'''
+
 S6 = getFourierN(g, 6)
 S7 = getFourierN(g, 7)
 S8 = getFourierN(g, 8)
@@ -51,23 +51,6 @@ p2[3].line_color = 'yellow'
 
 p2.show()
 '''
-
-H7 = getHaarN(g, 7)
-H8 = getHaarN(g, 8)
-
-p3 = plot(g, H7, H8, (t, -1, 1), show = False)
-p3[0].line_color = 'red'
-p3[0].line_color = 'blue'
-p3[0].line_color = 'yellow'
-
-print(H7)
-#plot(H7, (t,0,1))
-print(H7.subs(t,2).doit())
-#p3.show()
-
-#print(getFourierN(f, 6))
-#getHaarN(g, 6)
-#print(aux.subs(t, 0))
 
 # Ejercicio 3
 
