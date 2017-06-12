66741f46f0ef6dc04e77658b709819bc14359265
griger <gustavo.rg92@gmail.com>
2017-06-12 11:25:53 +0200

    Ejercicios sobre series de Fourier y aproximantes de Haar acabados.

--- fourier.py
+++ fourier.py
@@ -6,19 +6,27 @@ n,j,k = Symbol('n', integer = True), Symbol('j', integer = True), Symbol('k', in
 t = Symbol('t')
 psi = Piecewise((0, t < 0), (1, t < 0.5), (-1, t < 1), (0, True))
 
+'''Funcion que obtiene el termino an de la serie de Fourier para f
+:param p: El extremos superior del intervalo en el que definimos la funcion, [-p,p]
+'''
 def an (f, n, p = pi):
     return integrate(f*cos(n*t*pi/p), (t, -p, p)) / p
 
+'''Funcion que obtiene el termino bn de la serie de Fourier para f
+:param p: El extremos superior del intervalo en el que definimos la funcion, [-p,p]
+'''
 def bn (f, n, p = pi):
     return integrate(f*sin(n*t*pi/p), (t, -p, p)) / p
 
+'''Funcion que obtiene la serie de Fourier para f hasta el termino N-esimo.
+:param p: El extremos superior del intervalo en el que definimos la funcion, [-p,p]
+'''
 def getFourierN (f, N, p = pi):
-    return an(f, 0)/2 + summation(an(f,n) * cos(n*t*pi/p), (n, 1, N)) + summation(bn(f, n) * sin(n*t*pi/p), (n, 1, N))
-
-#Defino las funciones de los ejercicios
+    return an(f, 0,p)/2 + summation(an(f,n,p) * cos(n*t*pi/p), (n, 1, N)) + summation(bn(f, n,p) * sin(n*t*pi/p), (n, 1, N))
 
 # Ejercicio 1
 
+'''
 f = Piecewise((-1, t < 0), (1, t > 0), (0, True))
 S6 = getFourierN(f, 6)
 S7 = getFourierN(f, 7)
@@ -31,13 +39,12 @@ p1[2].line_color = 'green'
 p1[3].line_color = 'yellow'
 
 p1.show()
-
+'''
 
 # Ejercicio 2
 
 '''
 g = Piecewise((-t, t < 0), (t, 0 <= t), (0, True))
-
 
 S6 = getFourierN(g, 6)
 S7 = getFourierN(g, 7)
@@ -54,6 +61,16 @@ p2.show()
 
 # Ejercicio 3
 
-h = Piecewise((-6-t, And(-6 <= t, t < -3)), (t, And(-3 <= t, t < 3)), (6-t, And(3 <= t, t <= 6)))
+h = Piecewise((-6-t, t < -3), (t, t < 3), (6-t, t <= 6))
 
-# Ejercicio 4
+S6 = getFourierN(h, 6, 6)
+S7 = getFourierN(h, 7, 6)
+S8 = getFourierN(h, 8, 6)
+
+p3 = plot(h, S6, S7, S8, (t, -6, 6), legend = True, show = False)
+p3[0].line_color = 'red'
+p3[1].line_color = 'blue'
+p3[2].line_color = 'green'
+p3[3].line_color = 'yellow'
+
+p3.show()
--- haar.py
+++ haar.py
@@ -6,9 +6,11 @@ n,j,k = Symbol('n', integer = True), Symbol('j', integer = True), Symbol('k', in
 t = Symbol('t')
 psi = Piecewise((0, t < 0), (1, t < 0.5), (-1, t < 1), (0, True))
 
+'''Funcion que obtiene el aproximante de Haar j,k'''
 def getHaarAprox (jj,kk):
     return (2**(jj/2)) * psi.subs(t, 2**jj * t - kk)
 
+'''Funcion que obtiene el coeficiente j,k para la serie de Haar para la funcion f'''
 def ajk (f, jj, kk):
     
     if kk <= 2**jj -1:
@@ -16,18 +18,21 @@ def ajk (f, jj, kk):
     else:
         return 0
     #return Integral(f * getHaarAprox(jj,kk), (t,0,1)).evalf(maxn = 10)
-    
+
+'''Funcion que obtiene la serie de Haar hasta el termino N-esimo'''
 def getHaarN (f, N):
     return mp.nsum(lambda j,k : ajk(f,j,k) * getHaarAprox(j,k), [0,N], [0,N])
     #return summation(ajk(f,j,k) * getHaarAprox(j,k), (k, 0, 2**j - 1), (j, 0, N))
 
+#Ejercicio 2
+
 g = Piecewise( (-t, t < 0), (t, t >= 0) )
 
-H0 = getHaarN(g,0)
-H2 = getHaarN(g,0)
-H4 = getHaarN(g,0)
+H8 = getHaarN(g,8)
+H10 = getHaarN(g,10)
+H12 = getHaarN(g,5)
 
-p1 = plot(g, H0, H2, H4, (t, 0, 1), show = False)
+p1 = plot(g, H8, H10, H12, (t, 0, 1), show = False)
 p1[0].line_color = 'red'
 p1[1].line_color = 'blue'
 p1[2].line_color = 'green'
@@ -35,3 +40,21 @@ p1[3].line_color = 'yellow'
 
 p1.show()
 
+#Ejercicio 4
+
+'''
+h = Piecewise((-6-t, t < -3), (t, t < 3), (6-t, t <= 6))
+h = h.subs(t, 12*t - 6)
+
+H8 = getHaarN(h,8)
+H10 = getHaarN(h,10)
+H12 = getHaarN(h,12)
+
+p2 = plot(h, H8, H10, H12, (t, 0, 1), show = False)
+p2[0].line_color = 'red'
+p2[1].line_color = 'blue'
+p2[2].line_color = 'green'
+p2[3].line_color = 'yellow'
+
+p2.show()
+'''
